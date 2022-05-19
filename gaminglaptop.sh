7f3cc3486b0097fda5ff374e652d0b3b8562537a
somerandom@tld <somerandom@tld>
2022-05-18 19:07:17 -0700

    Narrow whitelist

--- gaminglaptop.sh
+++ gaminglaptop.sh
@@ -8,7 +8,7 @@ cd /home/pi/scripts/block.txt
 cat ~/scripts/block.txt/gaminglaptop.txt  | grep 0\.0\.0\.0\ .*[a-z] | awk '{print $2}' | sort | uniq > /tmp/gaminglaptop.tmp
 
 sqlite3 /etc/pihole/pihole-FTL.db 'select domain, COUNT (*) from queries where status in (1,4,5,6,7,8,9,10,11) AND client == "10.0.0.15" GROUP BY domain HAVING COUNT(*) > 6 ORDER BY COUNT(*) DESC' | \
-	sed -e 's/|.*//g' | grep -v -E 'conf.*unity3d.com|redditmedia|lastpass|www.google.com|chrome\.google\.com' >> /tmp/gaminglaptop.tmp
+	sed -e 's/|.*//g' | grep -v -E 'warthunder\.com|steam|discord|conf.*unity3d.com|redditmedia|lastpass|www.google.com|chrome\.google\.com' >> /tmp/gaminglaptop.tmp
 
 sort /tmp/gaminglaptop.tmp | uniq > /tmp/gaminglaptop.txt
 echo "# Generated on" $(date) "with" $(wc -l /tmp/gaminglaptop.txt | awk '{print $1}') "records" > ./gaminglaptop.txt
